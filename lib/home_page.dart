d84bb5c2aa4aa44f7bb3b4fcbb7fba8239d976c1
MohsenRahmdeli <mohsen.rahmdeli@gmail.com>
2024-12-29 23:22:00 +0400

    Compeleted Main

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -29,7 +29,6 @@ class _HomePageState extends State<HomePage> {
     55,
     66,
     77,
-    88,
     99,
     110,
     121,
@@ -55,84 +54,135 @@ class _HomePageState extends State<HomePage> {
     131,
     120,
     109,
-    98,
     87,
     76,
     65,
     54,
     43,
     32,
-    21
+    21,
+    78,
+    79,
+    80,
+    100,
+    101,
+    102,
+    84,
+    85,
+    86,
+    106,
+    107,
+    108,
+    24,
+    35,
+    46,
+    57,
+    30,
+    41,
+    52,
+    63,
+    81,
+    70,
+    59,
+    61,
+    72,
+    83,
+    26,
+    28,
+    37,
+    38,
+    39,
+    123,
+    134,
+    145,
+    156,
+    129,
+    140,
+    151,
+    162,
+    103,
+    114,
+    125,
+    105,
+    116,
+    127,
+    147,
+    148,
+    149,
+    158,
+    160,
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[800],
-      body: Column(
-        children: [
-          Expanded(
-            flex: 5,
-            child: Container(
-              child: GridView.builder(
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: numberOfSquares,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: numberRow),
-                itemBuilder: (BuildContext context, int index) {
-                  if(barriers.contains(index)){
-                    return Padding(
-                    padding: const EdgeInsets.all(1.0),
-                    child: Container(
-                      color: Colors.blue,
-                      child: Center(
-                        child: Text(
-                          index.toString(),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey[800],
+        body: Column(
+          children: [
+            Expanded(
+              flex: 5,
+              child: Container(
+                child: GridView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: numberOfSquares,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: numberRow),
+                  itemBuilder: (BuildContext context, int index) {
+                    if (barriers.contains(index)) {
+                      return Padding(
+                        padding: const EdgeInsets.all(1.0),
+                        child: Container(
+                          color: Colors.blue,
+                          child: Center(
+                            child: Text(
+                              index.toString(),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  );
-                  }else{
-                    return Padding(
-                    padding: const EdgeInsets.all(1.0),
-                    child: Container(
-                      color: Colors.grey,
-                      child: Center(
-                        child: Text(
-                          index.toString(),
+                      );
+                    } else {
+                      return Padding(
+                        padding: const EdgeInsets.all(1.0),
+                        child: Container(
+                          color: Colors.grey,
+                          child: Center(
+                            child: Text(
+                              index.toString(),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  );
-                  }
-                },
+                      );
+                    }
+                  },
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    'Score: ',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40,
+            Expanded(
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'Score: ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                      ),
                     ),
-                  ),
-                  Text(
-                    'P L A Y',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40,
+                    Text(
+                      'P L A Y',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
